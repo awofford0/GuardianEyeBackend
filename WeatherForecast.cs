4d224bc30ed0c8689bb14f73a3f44bf6a0be84b5
adamwofford <asw497@msstate.edu>
2023-10-20 14:52:33 -0500

    incorporated SQL database for weather forecasts

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace aspnetbackend
 {
@@ -7,6 +8,7 @@ namespace aspnetbackend
         public DateTime Date { get; set; }
 
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public int TemperatureC { get; set; }
 
